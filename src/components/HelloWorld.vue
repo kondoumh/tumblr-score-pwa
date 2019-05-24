c79b0dcb7e86203748aff8d42ff50e965f7474ff
kondoumh <kondoumh@gmail.com>
2019-05-24 13:23:54 +0900

    Add postItems

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,12 +9,25 @@
       <p>Posts : {{ posts }}</p>
       <p>URL : {{ url }}</p>
     </form>
+    <ul>
+      <li v-for="item in items" v-bind:key="item.id">
+        {{ item.url }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
+  mounted () {
+    if (localStorage.apiKey) {
+      this.apiKey = localStorage.apiKey
+    }
+    if (localStorage.blogIdentifier) {
+      this.blogIdentifier = localStorage.blogIdentifier
+    }
+  },
   props: {
     apiKey: String,
     blogIdentifier: String
@@ -22,7 +35,8 @@ export default {
   data: () => ({
     title: '',
     posts: 0,
-    url: ''
+    url: '',
+    items: []
   }),
   methods: {
     async fetchBlog () {
@@ -34,7 +48,20 @@ export default {
       this.url = blog['url']
       const response2 = await fetch(`https://api.tumblr.com/v2/blog/${this.blogIdentifier}/posts/?notes_info=true&reblog_info=true&offset=20&api_key=${this.apiKey}`)
       const data2 = await response2.json()
+      Object.keys(data2['response']['posts']).forEach(post => {
+        const postInfo = {
+          id: post['id'],
+          url: `'https://${this.blogIdentifier}/post/${post['id']}'`,
+          date: `'${post['date']}'`,
+          type: `'${post['type']}'`,
+          slug: `'${post['slug']}'`,
+          count: `'${post['note_count']}'`
+        }
+        this.items.push(postInfo)
+      })
       console.log(data2)
+      localStorage.apiKey = this.apiKey
+      localStorage.blogIdentifier = this.blogIdentifier
     }
   }
 }
